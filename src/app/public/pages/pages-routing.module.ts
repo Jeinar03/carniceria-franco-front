a1071a4a2ef459b308738e33281a974569fe7057
Jeinar03 <jesuseinar2003@gmail.com>
2026-09-03 22:18:55 -0600

    Limpieza F2: borra los componentes de gimnasio de pages/
    Componentes y rutas de la app de gimnasio que no son de la carnicería:
    blog-gird, blog-detail, testimontal, rutinas/ (públicas, users,
    preguntas-rutinas-test), grupos-musculares (+detalle, +video-detalle-gm),
    videos-all, videos-select, lesiones, guia-recuperacion. Varias ya estaban
    rotas (importadas en el routing pero sin declarar en pages.module).
    
    Servicios que solo usaban esos componentes: grupos-musculares, mis-rutinas,
    preguntas.
    
    pages-routing.module.ts y pages.module.ts reescritos: solo quedan productos,
    categorias, carrito, profile y seguimiento. Borrados también los huérfanos
    user-profile.module.ts / user-profile-routing.module.ts (el componente se usa
    vía pages.module). npm run build verde.

--- src/app/public/pages/pages-routing.module.ts
+++ src/app/public/pages/pages-routing.module.ts
@@ -1,119 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BlogDetailComponent } from './blog-detail/blog-detail.component';
-import { BlogGirdComponent } from './blog-gird/blog-gird.component';
-import { TestimontalComponent } from './testimontal/testimontal.component';
-import { CreateAccountComponent } from './create-account/create-account.component';
-import { RutinasPublicasComponent } from './rutinas/rutinas-public/rutinas-publicas/rutinas-publicas.component';
-import { RutinasUsersComponent } from './rutinas/rutinas-users/rutinas-users/rutinas-users.component';
 import { MisRutinasGuard } from 'src/guards/mis-rutinas/mis-rutinas.guard';
-import { RutinasPublicasDetalleComponent } from './rutinas/rutinas-public/rutinas-publicas-detalle/rutinas-publicas-detalle.component';
-import { GruposMuscularesComponent } from './grupos-musculares/grupos-musculares.component';
-import { GruposMuscularesDetalleComponent } from './grupos-musculares-detalle/grupos-musculares-detalle.component';
-import { VideoDetalleGMComponent } from './video-detalle-gm/video-detalle-gm.component';
-import {PreguntasRutinasTestComponent} from "./rutinas/preguntas-rutinas-test/preguntas-rutinas-test.component";
-import {UserProfileComponent} from "./user-profile/user-profile.component";
-import { VideosAllComponent } from './videos-all/videos-all.component';
-import { VideosSelectComponent } from './videos-select/videos-select.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 import { SeguimientoComponent } from './seguimiento/seguimiento.component';
-import { LesionesComponent } from './lesiones/lesiones.component';
-import { GuiaRecuperacionComponent } from './guia-recuperacion/guia-recuperacion.component';
 import { ProductosComponent } from './productos/productos.component';
 import { CategoriasComponent } from './categorias/categorias.component';
 import { CarritoComponent } from './carrito/carrito.component';
 
 const routes: Routes = [
   {
-    path: 'blog-gird',
-    component:BlogGirdComponent
-  },
-  {
-    path: 'blog-detail',
-    component:BlogDetailComponent
-  },
-  {
-    path: 'testimontal',
-    component:TestimontalComponent
-  },
-  {
-    path: 'create-account',
-    component:CreateAccountComponent
-  },
-  {
-    path: 'rutinas-publicas',
-    component:RutinasPublicasComponent
-  },
-  {
-    path: 'grupos-musculares',
-    component:GruposMuscularesComponent
-  },
-  {
-    path: 'seguimiento',
-    component:SeguimientoComponent,
-    canActivate: [MisRutinasGuard]
-  },
-  {
-    path: 'rutinas-publicas/:id',
-    component: RutinasPublicasDetalleComponent
-  },
-  {
-    path: 'grupos-musculares/video/:id',
-    component: VideoDetalleGMComponent
-  },
-  {
-    path: 'grupos-musculares/:id',
-    component: GruposMuscularesDetalleComponent
-  },
-  {
-    path: 'videos/:nombre',
-    component: VideosSelectComponent
-  },
-  {
-    path: 'rutinas-users',
-    component:RutinasUsersComponent,
-    canActivate: [MisRutinasGuard]
-  },
-  {
-    path: 'videos-all',
-    component:VideosAllComponent,
-
-  },
-  {
-    path: 'lesiones',
-    component:LesionesComponent,
-
-  },
-  {
-    path: 'guia-recuperacion',
-    component:GuiaRecuperacionComponent,
-
-  },
-  {
-    path: 'preguntas-rutinas-test',
-    component:PreguntasRutinasTestComponent,
-    canActivate: [MisRutinasGuard]
-  },
-  {
-    path: 'profile',
-    component:UserProfileComponent,
-    canActivate: [MisRutinasGuard]
-  },
-  {
     path: 'productos',
-    component: ProductosComponent
+    component: ProductosComponent,
   },
   {
     path: 'categorias',
-    component: CategoriasComponent
+    component: CategoriasComponent,
   },
   {
     path: 'carrito',
-    component: CarritoComponent
+    component: CarritoComponent,
+  },
+  {
+    path: 'profile',
+    component: UserProfileComponent,
+    canActivate: [MisRutinasGuard],
+  },
+  {
+    path: 'seguimiento',
+    component: SeguimientoComponent,
+    canActivate: [MisRutinasGuard],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-   exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PagesRoutingModule { }
+export class PagesRoutingModule {}
